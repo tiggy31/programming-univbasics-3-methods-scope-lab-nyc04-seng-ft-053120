13d5947681143b7dda32b3daebfff0631af48322
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-03 11:44:44 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,23 @@
+def all_phrases
+  
+  mario
+  toadstool
+  link
+end
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
+  #status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  response_of = 'It's Dangerous To Go Alone! Take This.'
+end
+#method is trying to put out a variable called phrase
+
